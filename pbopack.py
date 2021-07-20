a3c928a3cd4fb09e5a2e6990145494abdff42848
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2021-07-20 13:25:43 -0400

    add stat() check and template Pbo

--- pbopack.py
+++ pbopack.py
@@ -3,8 +3,24 @@
 # https://community.bistudio.com/wiki/PBO_File_Format
 
 import argparse
+import os
+import stat
 import sys
 
+
+class Pbo:
+    def __init__(self):
+        self.fileobj = None
+        self.filename = None
+        self.headers = []
+
+        self.properties = []
+
+    def unpack(self, pbo_file, dest):
+        pass
+
+    def pack(self, directory, pbo_file):
+        pass
 
 def main(args):
     parser = argparse.ArgumentParser(
@@ -29,6 +45,24 @@ If input is a file and no output is given, verifies the checksum of the file.
         help='specifies the file that contains the PBO properties (default .pboproperties)'
     )
     argspace, files = parser.parse_known_args(args)
+
+    if len(files) == 0 or len(files) > 2:
+        parser.print_help()
+        sys.exit(1)
+
+    infname = files[0]
+    outfname = files[1] if len(files) >= 2 else None
+
+    infmode = os.stat(infname).st_mode
+    if stat.S_ISREG(infmode):
+        pbo = Pbo()
+        pbo.unpack(infname, outfname)
+    elif stat.S_ISDIR(infmode):
+        pbo = Pbo()
+        pbo.pack(infname, outfname)
+    else:
+        print('error: %s is not a regular file nor a directory' % infname, file=sys.stderr)
+        sys.exit(1)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
